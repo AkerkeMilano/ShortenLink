f8b635a49c55eba49433ef60feca91066ec4ec98
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2022-08-16 15:22:38 +0600

    #4 add dispatch to post new links

--- link-frontend/src/container/Main.js
+++ link-frontend/src/container/Main.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
-// import { useSelector } from "react-redux";
-// import { useDispatch } from "react-redux";
-// import { postLinks } from "../store/actions/linksActions";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { postLinks } from "../store/actions/linksActions";
 
 const Main = () => {
     const [linkUrl, setLinkUrl] = useState("");
-    // const dispatch = useDispatch();
-    // const links = useSelector(state => state.links.links)
+    const dispatch = useDispatch();
+    const links = useSelector(state => state.links.links)
     const handleChange = (e) => {
         setLinkUrl(e.target.value);
     }
-    // const onSubmit = (e) => {
-    //     e.preventDefault();
-    //     dispatch(postLinks(linkUrl));
-    //     setLinkUrl("");
-    // }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        dispatch(postLinks(linkUrl));
+        setLinkUrl("");
+    }
     return (
         <div>
             <h2>Shorten your link!</h2>
