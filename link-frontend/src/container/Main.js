6296830b888b978f7b11f360176d8df0c946d115
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2022-08-16 15:43:02 +0600

    #4 add styling

--- link-frontend/src/container/Main.js
+++ link-frontend/src/container/Main.js
@@ -2,31 +2,48 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { postLinks } from "../store/actions/linksActions";
+import "./Main.css";
 
 const Main = () => {
-    const [linkUrl, setLinkUrl] = useState("");
-    const dispatch = useDispatch();
-    const links = useSelector(state => state.links.links)
-    const handleChange = (e) => {
-        setLinkUrl(e.target.value);
-    }
-    const onSubmit = (e) => {
+  const [linkUrl, setLinkUrl] = useState("");
+  const dispatch = useDispatch();
+  const links = useSelector((state) => state.links.links);
+  const handleChange = (e) => {
+    setLinkUrl(e.target.value);
+  };
+  const onSubmit = (e) => {
+    if(linkUrl.length !== 0){
         e.preventDefault();
         dispatch(postLinks(linkUrl));
         setLinkUrl("");
-    }
-    return (
+    } 
+  };
+  return (
+    <div>
+      <h1>Shorten your link!</h1>
+      <form onSubmit={onSubmit} className="link-form">
+        <input className="link-input" value={linkUrl} onChange={handleChange} required />
+
+        <button className="link-btn" type="submit">
+          Shorten!
+        </button>
+      </form>
+      {links.shortUrl ? (
         <div>
-            <h2>Shorten your link!</h2>
-            <form onSubmit={onSubmit}>
-                <input value={linkUrl} onChange={handleChange}/>
-                <button type="submit">Shorten</button>
-            </form>
-            <div>
-                <a href={`http://localhost:8000/${links.shortUrl}`} target="_blank" rel="noreferrer">{links.shortUrl}</a>
-            </div>
+          <h4>Your link now looks like this: </h4>
+          <a
+            href={`http://localhost:8000/${links.shortUrl}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {`http://localhost:8000/${links.shortUrl}`}
+          </a>
         </div>
-    )
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default Main;
