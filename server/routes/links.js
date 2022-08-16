ff6f02b4de7c23acfd8aa53f236a696ca978cf28
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2022-08-16 14:48:46 +0600

    #2 create GET request

--- server/routes/links.js
+++ server/routes/links.js
@@ -6,6 +6,7 @@ const Link = require("../models/Link");
 const createRouter = () => {
     router.post("/links", async (req, res) => {
         const link = new Link(req.body);
+        link.shortUrl = nanoid(7);
         try{
             await link.save();
             res.send(link);
@@ -14,6 +15,14 @@ const createRouter = () => {
             res.status(400).send({e});
         }
     });
+    router.get("/:shortUrl", async (req, res) => {
+        try{
+            const link = await Link.findOne({shortUrl: req.params.shortUrl});
+            res.status(301).redirect(link.originalUrl)
+        } catch(e){
+            res.sendStatus(404);
+        }
+    })
     return router;
 }
 
